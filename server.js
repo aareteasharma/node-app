88f85acddec9462876c434bf08f0bb5f154feb6d
aareteasharma <90687658+aareteasharma@users.noreply.github.com>
2021-09-19 13:07:33 +0530

    Update | New Version 14

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:Blue;">AWS Pipline Test - version-13!!*********</h1> \n');
+  res.send('<h1 style="color:Red;">********AWS Pipline Test - version-14!!*********</h1> \n');
 });
 
 app.listen(PORT, HOST);
